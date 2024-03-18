9376c1d758a015b768ee63cf1e0f5f3d36d91479
MillenaFerreira22 <millenaff.27@gmail.com>
2024-03-18 08:26:36 -0300

    formulario

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var botao = document.getElementById("botao")
+const botao = document.getElementById("botao")
 botao.addEventListener("click", alertar);
 
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 function alertar(){
-    alert("O botão foi clicado!!!");
+    //alert("O nome inserido foi" + nome.value);
+    
+    saida.innerText= "Nome: " + nome.value +
+              "\n E-mail: " + email.value + 
+              "\n Telefone: " + telefone.value; + 
+              "\n CEP: " + cep.value +
+              "\n telefone: " + logradouro.value +
+              "\n : " + complemento.value +
+              "\n E-mail: " + logradouro.value +
+              "\n E-mail: " + numero.value +
+    
 }
